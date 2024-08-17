3e0957b91bf115f89899f606136dfe53e27156db
Abubakar <abubakkeramjad10@gmail.com>
2024-08-18 00:18:47 +0500

    setup openai api key

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-# to install local package in our virtual environment 
+# to install dependencies locally in our virtual environment 
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='abubakar',
+    author_email='abubakkeramjad10@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
+# responsible for finding the local packages hint: __init__.py => find_packages()
